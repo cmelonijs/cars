3a1de3767c7bf6599420641214e63406c6487b74
cmeloni <carlo.meloni@abinsula.com>
2023-06-14 10:54:18 +0200

    feat: adding images to public folder and adding hero images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,6 +12,12 @@ const Hero = () => {
                 <h1 className='hero__title'>Find, book, or rent a car -- quickly and easily.</h1>
                 <p className='hero__subtitle'>Streamless your car rental exprerience with our effortless booking process.</p>
                 <CustomButton title="Explore cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}  />
+                <div className='hero__image-container'>
+                    <div className='hero__image'>
+                        <Image src="/hero.png" alt="hero" fill className='object-contain' />
+                        <div className='hero__image-overlay' />
+                    </div>
+                </div>
             </div>
         </div>
     )
